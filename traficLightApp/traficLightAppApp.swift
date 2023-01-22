90248eec3461b5cc77c1caeb20836dbe42fea73d
nmk098 <nkuryumov@gmail.com>
2023-01-22 22:29:04 +0400

    Initial Commit

---
+++ traficLightApp/traficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  traficLightAppApp.swift
+//  traficLightApp
+//
+//  Created by Никита Курюмов on 22.01.23.
+//
+
+import SwiftUI
+
+@main
+struct traficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
